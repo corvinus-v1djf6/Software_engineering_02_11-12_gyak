e77c02ce9cac4f3706dd2501866e407c8c097e85
Balázs Schmidt <balazs.schmidt2@stud.uni-corvinus.hu>
2022-12-01 23:59:31 +0100

    Add project files.

---
+++ 11-12_gyak/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _11_12_gyak.Controllers
+{
+    //Route("api/[controller]")
+    [ApiController]
+
+    public class BoatController : Controller  //ide lehet kell a Base
+    {
+        [HttpGet]
+        [Route("questions/all")]
+
+        public IActionResult MindegyHogyHivjak()
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
